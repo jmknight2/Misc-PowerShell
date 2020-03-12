8023662d992f0d5b66cd45e85e8ae889fb7b672e
Jon Knight <donkey.68@live.com>
2020-03-12 16:19:12 -0400

    Moved hardcoded paths into an array for ease.

--- Clear-TeamsCache.ps1
+++ Clear-TeamsCache.ps1
@@ -1,3 +1,13 @@
+$CachePaths = @(
+    '\Microsoft\teams\application cache\cache',
+    '\Microsoft\teams\blob_storage',
+    '\Microsoft\teams\Cache',
+    '\Microsoft\teams\databases',
+    '\Microsoft\teams\GPUcache',
+    '\Microsoft\teams\Local Storage',
+    '\Microsoft\teams\tmp'
+)
+
 $Continue = [System.Windows.Forms.MessageBox]::Show("To clear cache, we must close temporarily close Teams.`nDo you wish to continue?","Warning", "YesNo" , "Warning" , "Button1")
 
 if($Continue -eq 'Yes') {
@@ -6,71 +16,25 @@ if($Continue -eq 'Yes') {
 
     $TotalKBToDelete = 0
 
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\application cache\cache')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\application cache\cache') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\blob_storage')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\blob_storage') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\Cache')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\Cache') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\databases')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\databases') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\GPUcache')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\GPUcache') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
+    foreach($Path in $CachePaths) {
+        if(test-path -Path (Join-Path $($env:APPDATA) $Path)) {
+            Get-ChildItem -Path (Join-Path $($env:APPDATA) $Path) -Recurse | Foreach-Object {
+                $TotalKBToDelete += ($_.Length / 1024)
+                $_ | Remove-Item -Force
+                Write-Verbose "Removed $($_.Name)"
+            }
         }
     }
 
     if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\IndexedDB')) {
         (Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\IndexedDB') -Recurse).Where{$_.Extension -eq '.db'} | Foreach-Object {
             $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\Local Storage')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\Local Storage') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
-            Write-Verbose "Removed $($_.Name)"
-        }
-    }
-
-    if(test-path -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\tmp')) {
-        Get-ChildItem -Path (Join-Path $($env:APPDATA) '\Microsoft\teams\tmp') -Recurse | Foreach-Object {
-            $TotalKBToDelete += ($_.Length / 1024)
-            Remove-Item $_ -Force
+            $_ | Remove-Item -Recurse -Force
             Write-Verbose "Removed $($_.Name)"
         }
     }
 
     Start-Process -FilePath (Join-Path $env:LOCALAPPDATA '\Microsoft\Teams\Update.exe') -ArgumentList '-processStart "Teams.exe"'
 
-    [void]([System.Windows.Forms.MessageBox]::Show("Cache cleared successfully. `nWe removed $($TotalKBToDelete)KB of cache data.","Information", "Ok" , "Information" , "Button1"))
+    [void]([System.Windows.Forms.MessageBox]::Show("Cache cleared successfully. `nWe removed $($TotalKBToDelete / 1024)MB of cache data.","Information", "Ok" , "Information" , "Button1"))
 }
